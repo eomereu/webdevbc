7fe99bc89ba03265fb9f84f7047b35e68fc279ae
Ersegun Omer EROL <eomereu@gmail.com>
2020-08-03 07:09:11 +0200

    JS Stalker Exercise

---
+++ 06_Intro_to_JS/02_JS_Stalker/script.js
@@ -0,0 +1,8 @@
+var firstName = prompt("Was ist Ihr Vorname?");
+var lastName = prompt("Was ist Ihr Nachname?");
+var age = prompt("Wie alt sind Sie?");
+
+var fullName = firstName + " " + lastName;
+
+console.log("Ihr vollständiger Name ist " + fullName);
+console.log("Sie sind " + age + " Jahre alt!");
