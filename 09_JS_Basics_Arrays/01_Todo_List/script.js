f7c2c0dfdd6c7fdec98b8eec4b83f3c9182747af
Ersegun Omer EROL <eomereu@gmail.com>
2020-08-14 17:08:57 +0200

    Todo List Part-2

--- 09_JS_Basics_Arrays/01_Todo_List/script.js
+++ 09_JS_Basics_Arrays/01_Todo_List/script.js
@@ -3,12 +3,35 @@ var todos = [];
 window.setTimeout(function() {
   while(true) {
     var choice = prompt("Was hättest du machen?");
-    if(choice === "auflisten") console.log(todos);
+    if(choice === "auflisten") {
+      listTodos();
+    }
     else if(choice === "verlassen") break;
     else if(choice === "neu") {
-      var newItem = prompt("Screiben Sie eine neue Aufgabe");
-      todos.push(newItem);
+      addTodo();
+    }
+    else if(choice === "löschen") {
+      deleteTodo();
     }
     else alert("Machen Sie eine gültige Wahl bitte!");
+  }
+
+  //My Functions
+  function listTodos() {
+    console.log("**********");
+    todos.forEach(function(el, i){
+      console.log(i + ": " + el);
+    });
+    console.log("**********");
+  }
+  function deleteTodo() {
+    var itemIndex = prompt("Was war der Index?");
+    todos.splice(itemIndex, 1);
+    console.log("Aufgabe wird gelöscht");
+  }
+  function addTodo() {
+    var newItem = prompt("Screiben Sie eine neue Aufgabe");
+    todos.push(newItem);
+    console.log("\"" + newItem + "\" wird der Aufgabenliste hinzugefügt")
   }
 }, 500);
