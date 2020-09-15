f07a4e458e70eef57beb07f6515b4f7acba1cb66
Ersegun Omer EROL <eomereu@gmail.com>
2020-09-15 16:55:25 +0200

    Color Guessing Game Project - Part 6

--- 13_Color_Game/script.js
+++ 13_Color_Game/script.js
@@ -1,17 +1,17 @@
-// --- VARIABLES --- \\
+// ___ELEMENTS___ \\
 var squares = document.querySelectorAll(".square");
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
-var resetButton = document.querySelector("#reset");
+var resetBtn = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easy");
+var hardBtn = document.querySelector("#hard");
 
-var colors = generateRandomColors(6);
+
+// ___MAIN___ \\
+var numSquares = 6; //by default it's hard mode so there must be 6 squares
+var colors = generateRandomColors(numSquares);
 var pickedColor = pickColor();
-
-var alreadyWon = false;
-
-
-// --- MAIN --- \\
 colorDisplay.textContent = pickedColor;
 colorDisplay.style = "text-transform: uppercase;";
 
@@ -28,7 +28,7 @@ function colorizeSquares(){
 }
 
 function functionalizeThings(){
-  // --- Functionalize Squares --- \\
+  // ___Functionalize Squares___ \\
   for(var i = 0; i < squares.length; i++){
     // add click listeners to squares
     squares[i].addEventListener("click", function(){
@@ -40,28 +40,63 @@ function functionalizeThings(){
         messageDisplay.textContent = "Correct!";
         changeColors(clickedColor);
         h1.style.backgroundColor = clickedColor;
-        resetButton.textContent = "Play Again?";
+        resetBtn.textContent = "Play Again?";
       } else {
         this.style.backgroundColor = "#232323";
         messageDisplay.textContent = "Try Again!";
       }
     });
   }
-  // --- Functionalize Reset Button --- \\
-  resetButton.addEventListener("click", function(){
-    // generate all new colors
-    colors = generateRandomColors(6);
-    // pick a new random color from array
-    pickedColor = pickColor();
-    // change the displayed color content
-    colorDisplay.textContent = pickedColor;
-    // reset textContent of the ResetButton
-    resetButton.textContent = "New Colors";
-    // reset the h1 background
-    h1.style.backgroundColor = "#232323";
-    // change colors of the squares
+  // ___Functionalize Reset Button___ \\
+  resetBtn.addEventListener("click", function(){
+    // generate all new colors based on the selected game mode (number of squares actually), colorize squares and pick a new color
+    colors = generateRandomColors(numSquares);
     colorizeSquares();
+    pickedColor = pickColor();
+    // change/reset the displays/visuals
+    changeDisplaysViuals();
   });
+  // ___Functionalize Easy Button___ \\
+  easyBtn.addEventListener("click", function(){
+    // arrange the selected styling
+    this.classList.add("selected");
+    hardBtn.classList.remove("selected");
+    // create 3 colors colorozing only first 3 squares and pick a new color
+    numSquares = 3;
+    colors = generateRandomColors(numSquares);
+    colorizeSquares(); //here it won't recolorize the last 3 square! It's not a big deal because we'll hide them anyway...
+    pickedColor = pickColor();
+    // change/reset the displays/visuals
+    changeDisplaysViuals();
+    // hide last 3 non-remodified squares
+    squares[3].style.display = "none";
+    squares[4].style.display = "none";
+    squares[5].style.display = "none";
+  });
+  // ___Functionalize Hard Button___ \\
+  hardBtn.addEventListener("click", function(){
+    this.classList.add("selected");
+    easyBtn.classList.remove("selected");
+    // create 6 colors, colorizing all squares again and pick a new color
+    numSquares = 6;
+    colors = generateRandomColors(numSquares);
+    colorizeSquares();
+    pickedColor = pickColor();
+    // change/reset the displays/visuals and the game mode
+    changeDisplaysViuals();
+    gameModeHard = true;
+    // unhide the last 3 squares
+    squares[3].style.display = "block";
+    squares[4].style.display = "block";
+    squares[5].style.display = "block";
+  });
+}
+
+function changeDisplaysViuals(){
+  colorDisplay.textContent = pickedColor;
+  messageDisplay.textContent = "";
+  h1.style.backgroundColor = "#232323";
+  resetBtn.textContent = "New Colors";
 }
 
 function changeColors(color){
