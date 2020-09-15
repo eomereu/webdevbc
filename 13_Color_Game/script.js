e89c6605ce1b43f90db22b8a5c72deda2c72a723
Ersegun Omer EROL <eomereu@gmail.com>
2020-09-15 16:05:15 +0200

    Color Guessing Game Project - Part 5

--- 13_Color_Game/script.js
+++ 13_Color_Game/script.js
@@ -1,32 +1,66 @@
-var colors = generateRandomColors(6);
-
+// --- VARIABLES --- \\
 var squares = document.querySelectorAll(".square");
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+
+var colors = generateRandomColors(6);
 var pickedColor = pickColor();
 
+var alreadyWon = false;
+
+
+// --- MAIN --- \\
 colorDisplay.textContent = pickedColor;
 colorDisplay.style = "text-transform: uppercase;";
 
-for(var i = 0; i < squares.length; i++){
-  // add initial colors to squares
-  squares[i].style.backgroundColor = colors[i];
+// colorize squares for the first time
+colorizeSquares();
+functionalizeThings();
 
-  // add click listeners to squares
-  squares[i].addEventListener("click", function(){
-    // grab color of clicked square
-    var clickedColor = this.style.backgroundColor;
-    // compare color to pickedColor
-    console.log(clickedColor, pickedColor);
-    if(clickedColor === pickedColor) {
-      messageDisplay.textContent = "Correct!";
-      changeColors(clickedColor);
-      h1.style.backgroundColor = clickedColor;
-    } else {
-      this.style.backgroundColor = "#232323";
-      messageDisplay.textContent = "Try Again!";
-    }
+
+// ___FUNCTIONS___ \\
+function colorizeSquares(){
+  for(var i = 0; i < squares.length; i++){
+    squares[i].style.backgroundColor = colors[i];
+  }
+}
+
+function functionalizeThings(){
+  // --- Functionalize Squares --- \\
+  for(var i = 0; i < squares.length; i++){
+    // add click listeners to squares
+    squares[i].addEventListener("click", function(){
+      // grab color of clicked square
+      var clickedColor = this.style.backgroundColor;
+      // compare color to pickedColor
+      console.log(clickedColor, pickedColor);
+      if(clickedColor === pickedColor) {
+        messageDisplay.textContent = "Correct!";
+        changeColors(clickedColor);
+        h1.style.backgroundColor = clickedColor;
+        resetButton.textContent = "Play Again?";
+      } else {
+        this.style.backgroundColor = "#232323";
+        messageDisplay.textContent = "Try Again!";
+      }
+    });
+  }
+  // --- Functionalize Reset Button --- \\
+  resetButton.addEventListener("click", function(){
+    // generate all new colors
+    colors = generateRandomColors(6);
+    // pick a new random color from array
+    pickedColor = pickColor();
+    // change the displayed color content
+    colorDisplay.textContent = pickedColor;
+    // reset textContent of the ResetButton
+    resetButton.textContent = "New Colors";
+    // reset the h1 background
+    h1.style.backgroundColor = "#232323";
+    // change colors of the squares
+    colorizeSquares();
   });
 }
 
