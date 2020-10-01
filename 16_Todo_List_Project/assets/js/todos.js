b8ea04d555ae7921cb66d3268bc2db4f7bd249ad
Ersegun Omer EROL <eomereu@gmail.com>
2020-10-02 00:46:31 +0200

    Deleting Todos

--- 16_Todo_List_Project/assets/js/todos.js
+++ 16_Todo_List_Project/assets/js/todos.js
@@ -1,3 +1,12 @@
+// Check odd specific todos by clicking
 $("li").on("click", function(){
   $(this).toggleClass("done");
+});
+
+// Click on X to delete Todos
+$(".delBtn").click(function(e){
+  $(this).parent().fadeOut(400, function(){
+    $(this).remove();
+  });
+  e.stopPropagation();
 });
